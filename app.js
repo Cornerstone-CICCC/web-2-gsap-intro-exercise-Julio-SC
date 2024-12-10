b6fe5399c0c74bbf7626cdc65a182b2f46cfe784
Julio-SC <jhver2001@gmail.com>
2024-12-10 15:06:30 -0800

    exercise completed

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
 // PUT YOUR CODE HERE
+const squareTimeLine = gsap.timeline();
+
+
+
+
+squareTimeLine
+.from('.red ', {x: '-100%', y: '-100%', duration: 1, delay: 1})
+.from('.green ', {x: '100%', y: '-100%', duration: 1})
+.from('.blue ', {x: '100%', y: '100%', duration: 1})
+.from('.yellow ', {x: '-100%', y: '100%', duration: 1})
+.to('.red', { left: "calc(100% - 200px", duration: 1})
+.to('.green', { top: "calc(100% - 200px", duration: 1}, '<')
+.to('.blue', { right: "calc(100% - 200px", duration: 1}, '<')
+.to('.yellow', { bottom: "calc(100% - 200px", duration: 1}, '<')
+.to('.box', {borderRadius: "50%", duration: 1}, '<')
+.to('.red ', {x: '100%', duration: 1}, '<')
+.to('.green ', {x: '100%', duration: 1}, '<')
+.to('.blue ', {x: '-100%', duration: 1}, '<')
+.to('.yellow ', {x: '-100%', duration: 1}, '<')
+
+
+  squareTimeLine.play()
+
+  c
